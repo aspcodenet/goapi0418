e6caef9e00ccdbcf24330cd3da64e0b80f9bba22
Stefan Holmberg <stefan.holmberg@systementor.se>
2024-04-25 16:36:48 +0200

    v2

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"main/data"
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
@@ -46,6 +47,34 @@ func handleNewEmployees(c *gin.Context) {
 	}
 }
 
+func apiEmployeeUpdateById(c *gin.Context) {
+	id := c.Param("id")
+	var employee data.Employee
+	err := data.DB.First(&employee, id).Error
+	if errors.Is(err, gorm.ErrRecordNotFound) {
+		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "not found"})
+	} else {
+		if err := c.BindJSON(&employee); err != nil {
+			return
+		}
+		employee.Id, _ = strconv.Atoi(id)
+		data.DB.Save(&employee)
+		c.IndentedJSON(http.StatusOK, employee)
+	}
+}
+
+func apiEmployeeDeleteById(c *gin.Context) {
+	id := c.Param("id")
+	var employee data.Employee
+	err := data.DB.First(&employee, id).Error
+	if errors.Is(err, gorm.ErrRecordNotFound) {
+		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "not found"})
+	} else {
+		data.DB.Delete(&employee)
+		c.IndentedJSON(http.StatusNoContent, employee)
+	}
+}
+
 func handleStartPage(c *gin.Context) {
 	c.Data(http.StatusOK, "text/html; charset=utf-8", []byte("<html><body><h1>Hej hopp</h1><ul><li>Test</li><li>Bla</li></ul></body></html>"))
 }
@@ -75,6 +104,9 @@ func main() {
 	r.GET("/api/employee", handleGetEmployees)
 	r.GET("/api/employee/:id", handleOneEmployee)
 	r.POST("/api/employee", handleNewEmployees)
+	r.PUT("/api/employee/:id", apiEmployeeUpdateById)
+
+	r.DELETE("/api/employee/:id", apiEmployeeDeleteById)
 
 	r.Run(":8080")
 
